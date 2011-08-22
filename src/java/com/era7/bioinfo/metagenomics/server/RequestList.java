623a1697f7c566c67e4ecc24ccbaa7b5c9a6534e
Pablo Pareja Tobes <ppareja@era7.com>
2011-08-22 12:33:12 +0200

    first commit

---
+++ src/java/com/era7/bioinfo/metagenomics/server/RequestList.java
@@ -0,0 +1,20 @@
+package com.era7.bioinfo.metagenomics.server;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author ppareja
+ */
+public class RequestList {
+
+    public static final String GET_SAMPLES_REQUEST = "get_samples";
+    public static final String GET_TAXON_CHILDREN_REQUEST = "get_taxon_children";
+    public static final String GET_WHOLE_TAXONOMY_TREE_REQUEST = "get_whole_taxonomy_tree";
+
+}
