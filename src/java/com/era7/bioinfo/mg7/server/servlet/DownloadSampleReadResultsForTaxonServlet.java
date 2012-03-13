90e372fac4a0cb30d12147afee30c4ac4d1c23d1
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2012-03-13 11:51:50 +0100

    Adding new features for multifasta download of reads per taxon (with/without descendants)

--- src/java/com/era7/bioinfo/mg7/server/servlet/DownloadSampleReadResultsForTaxonServlet.java
+++ src/java/com/era7/bioinfo/mg7/server/servlet/DownloadSampleReadResultsForTaxonServlet.java
@@ -13,6 +13,7 @@ import com.era7.bioinfo.mg7.relationships.ReadResultNcbiTaxonRel;
 import com.era7.bioinfo.mg7.relationships.ReadResultSampleRel;
 import com.era7.bioinfo.mg7.server.CommonData;
 import com.era7.bioinfo.mg7.server.RequestList;
+import com.era7.lib.bioinfo.bioinfoutil.fasta.FastaUtil;
 import com.era7.lib.bioinfoxml.metagenomics.ReadResultXML;
 import com.era7.lib.bioinfoxml.metagenomics.SampleXML;
 import com.era7.lib.bioinfoxml.ncbi.NCBITaxonomyNodeXML;
@@ -76,6 +77,8 @@ public class DownloadSampleReadResultsForTaxonServlet extends HttpServlet {
             if (method.equals(RequestList.DOWNLOAD_SAMPLE_READ_RESULTS_FOR_TAXON_REQUEST)) {
 
                 String fileName = myReq.getParameters().getChildText("file_name");
+                String format = myReq.getParameters().getChildText("format");
+                Boolean includeDescendants = Boolean.parseBoolean(myReq.getParameters().getChildText("include_descendants"));
 
                 System.out.println(myReq.getMethod());
 
@@ -87,45 +90,27 @@ public class DownloadSampleReadResultsForTaxonServlet extends HttpServlet {
                 NodeRetriever nodeRetriever = new NodeRetriever(manager);
 
                 StringBuilder stBuilder = new StringBuilder();
-                stBuilder.append(("<read_results sample=\"" + sampleXML.getSampleName()
-                        + "\" taxon_id=\"" + taxonXML.getTaxId() + "\" >\n"));
+
+                if (format.equals("xml")) {
+                    stBuilder.append(("<read_results sample=\"" + sampleXML.getSampleName()
+                            + "\" taxon_id=\"" + taxonXML.getTaxId() + "\" >\n"));
+                }
 
                 ReadResultSampleRel readResultSampleRel = new ReadResultSampleRel(null);
 
-                NCBITaxonNode taxonNode = nodeRetriever.getNCBITaxonByTaxId("" + taxonXML.getTaxId());
+                Iterator<Node> sampleIterator = manager.getSampleNameIndex().get(SampleNode.SAMPLE_NAME_INDEX, sampleXML.getSampleName()).iterator();
 
-                Iterator<Node> iterator = manager.getSampleNameIndex().get(SampleNode.SAMPLE_NAME_INDEX, sampleXML.getSampleName()).iterator();
-                if (iterator.hasNext()) {
+                if (sampleIterator.hasNext()) {
 
-                    SampleNode sampleNode = new SampleNode(iterator.next());
-                    Iterator<Relationship> relIterator = taxonNode.getNode().getRelationships(new ReadResultNcbiTaxonRel(null), Direction.INCOMING).iterator();
+                    SampleNode sampleNode = new SampleNode(sampleIterator.next());
 
-                    while (relIterator.hasNext()) {
+                    NCBITaxonNode taxonNode = nodeRetriever.getNCBITaxonByTaxId("" + taxonXML.getTaxId());
 
-                        ReadResultNode readResultsNode = new ReadResultNode(relIterator.next().getStartNode());
-                        SampleNode tempSampleNode = new SampleNode(readResultsNode.getNode().getSingleRelationship(readResultSampleRel, Direction.OUTGOING).getEndNode());
+                    getReadsAndWriteThemToFile(taxonNode, format, stBuilder, includeDescendants, sampleNode, readResultSampleRel);
 
-                        if (sampleNode.getName().equals(tempSampleNode.getName())) {
-
-                            ReadResultXML readResultXML = new ReadResultXML();
-                            readResultXML.setReadId(readResultsNode.getReadId());
-                            readResultXML.setIdentity(readResultsNode.getIdentity());
-                            readResultXML.setQueryLength(readResultsNode.getQueryLength());
-                            readResultXML.setEvalue(readResultsNode.getEvalue());
-                            readResultXML.setGiId(readResultsNode.getGiId());
-                            readResultXML.setHitLength(readResultsNode.getHitLength());
-                            readResultXML.setAlignmentLength(readResultsNode.getAlignmentLength());
-                            readResultXML.setMidline(readResultsNode.getMidline());
-                            readResultXML.setQuerySequence(readResultsNode.getQuerySequence());
-                            readResultXML.setHitSequence(readResultsNode.getHitSequence());
-
-                            stBuilder.append((readResultXML.toString() + "\n"));
-
-                        }
-
+                    if (format.equals("xml")) {
+                        stBuilder.append("</read_results>");
                     }
-
-                    stBuilder.append("</read_results>");
 
                     System.out.println("writing response");
 
@@ -140,12 +125,12 @@ public class DownloadSampleReadResultsForTaxonServlet extends HttpServlet {
                     response.setContentLength(byteArray.length);
 
                     System.out.println("doneee!!");
-                    
+
                     System.out.println("end reached!");
-                    
+
                     //out.flush();
-                    
-                    
+
+
 
                 } else {
                     Response resp = new Response();
@@ -155,11 +140,58 @@ public class DownloadSampleReadResultsForTaxonServlet extends HttpServlet {
                 }
 
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             Logger.getLogger(DownloadSampleReadResultsForTaxonServlet.class.getName()).log(Level.SEVERE, null, e);
         }
 
         out.close();
 
+    }
+
+    protected void getReadsAndWriteThemToFile(NCBITaxonNode taxonNode,
+            String format,
+            StringBuilder stBuilder,
+            Boolean includeDescendants,
+            SampleNode sampleNode,
+            ReadResultSampleRel readResultSampleRel) {
+
+        Iterator<Relationship> relIterator = taxonNode.getNode().getRelationships(new ReadResultNcbiTaxonRel(null), Direction.INCOMING).iterator();
+
+        while (relIterator.hasNext()) {
+
+            ReadResultNode readResultsNode = new ReadResultNode(relIterator.next().getStartNode());
+            SampleNode tempSampleNode = new SampleNode(readResultsNode.getNode().getSingleRelationship(readResultSampleRel, Direction.OUTGOING).getEndNode());
+
+            if (sampleNode.getName().equals(tempSampleNode.getName())) {
+
+                if (format.equals("xml")) {
+                    ReadResultXML readResultXML = new ReadResultXML();
+                    readResultXML.setReadId(readResultsNode.getReadId());
+                    readResultXML.setIdentity(readResultsNode.getIdentity());
+                    readResultXML.setQueryLength(readResultsNode.getQueryLength());
+                    readResultXML.setEvalue(readResultsNode.getEvalue());
+                    readResultXML.setGiId(readResultsNode.getGiId());
+                    readResultXML.setHitLength(readResultsNode.getHitLength());
+                    readResultXML.setAlignmentLength(readResultsNode.getAlignmentLength());
+                    readResultXML.setMidline(readResultsNode.getMidline());
+                    readResultXML.setQuerySequence(readResultsNode.getQuerySequence());
+                    readResultXML.setHitSequence(readResultsNode.getHitSequence());
+
+                    stBuilder.append((readResultXML.toString() + "\n"));
+
+                } else if (format.equals("multifasta")) {
+
+                    stBuilder.append((">" + readResultsNode.getReadId() + "\n" + FastaUtil.formatSequenceWithFastaFormat(readResultsNode.getHitSequence(), 60) + "\n"));
+
+                }
+            }
+
+        }
+        
+        if(includeDescendants){
+            for (NCBITaxonNode child : taxonNode.getChildren()) {
+                getReadsAndWriteThemToFile(child, format, stBuilder, includeDescendants, sampleNode, readResultSampleRel);
+            }            
+        }
     }
 }
--- src/java/com/era7/bioinfo/mg7/server/servlet/GetWholeTaxonomyTreeForSampleServlet.java
+++ src/java/com/era7/bioinfo/mg7/server/servlet/GetWholeTaxonomyTreeForSampleServlet.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
